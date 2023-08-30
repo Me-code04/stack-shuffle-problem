0b65ac094c75d6518cf880b33d2d0310eeb04d2e
Suhrid Sadman Abrar <suhridabrarbd@outlook.com>
2023-08-30 21:28:15 +0530

    Changes to simplify the code.

--- deckshuffle.py
+++ deckshuffle.py
@@ -24,39 +24,11 @@ def shuffle(deck):
 def are_consecutive(x, y):
     return np.abs(x - y) == 1
 
-''''consecutive_count = 0
-
-def how_many_shuffles(deck):
-    global num_cards
-    global consecutive_count
-    num_shuffles = 0
-    for i in range(1, len(deck)):
-        if are_consecutive(deck[i-1], deck[i]) and consecutive_count < num_cards:
-            #print(f"{deck[i-1]} and {deck[i]} are consecutive.")
-            deck = riffle_shuffle(deck)
-            num_shuffles += 1
-            consecutive_count += 1
-            #print("Shuffling...")
-            #print(deck)
-            #print(consecutive_count)
-        elif consecutive_count < num_cards:
-            #print(f"{deck[i-1]} and {deck[i]} are not consecutive.")
-            #print("Final shuffled deck:", deck)
-            #print(consecutive_count)
-            if consecutive_count == num_cards:
-                return num_shuffles
-    
-    # Add this return statement when no more consecutive cards are left to shuffle
-    return num_shuffles'''
-
 def is_homogeneous(deck):
     for i in range(len(deck)):
         if are_consecutive(deck[i], deck[(i + 1) % num_cards]):
             return False
     return True
-
-# Simulate shuffling until a homogeneous deck is achieved
-
 
 # Simulate the shuffling process multiple times
 required_shuffles_values = []
@@ -72,7 +44,7 @@ for _ in range(num_simulations):
 bin_width = 5
 bins = np.arange(0, max(required_shuffles_values) + bin_width, bin_width)
 plt.hist(required_shuffles_values, bins=bins, edgecolor='black')
-plt.title(f"Frequency Distribution of Required Shuffles for a Homogeneous Deck")
+plt.title("Frequency Distribution of Required Shuffles for a Homogeneous Deck")
 plt.xlabel("Number of Required Shuffles")
 plt.ylabel("Frequency")
 plt.savefig("shuffles_histogram.png")
